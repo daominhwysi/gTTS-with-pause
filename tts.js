a1b3bbe8fa5f4b6aedf9ffcdbaa54dfa445dc21f
DaoMinhWysi <114815151+DaoMinhWysi@users.noreply.github.com>
2023-05-30 03:50:56 -0700

    idk

---
+++ tts.js
@@ -0,0 +1,35 @@
+const gtts = require('gtts');
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf8');
+if (data == '') {
+    console.log('Input Text in input.txt file')
+}
+console.log(data);
+
+const sentences = data.split(/[.?!]/g).filter(Boolean);
+
+dotCount = sentences.length
+console.log(dotCount)
+dotCount++
+let filenumber = 1
+
+for (let sentence of sentences) {
+    const gttsVoice = new gtts(sentence, 'vi');
+     if ( dotCount > filenumber) {
+      gttsVoice.save(`audio/ ${filenumber}.mp3`, function(err, result) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      console.log(`${filenumber}.mp3`)
+      filenumber++;
+    }
+  }
+const content = `${filenumber}`;
+fs.writeFile('cache/filenumber.txt', content, err => {
+  if (err) {
+    console.error(err);
+     return;
+  }
+  });
+console.log(filenumber)
